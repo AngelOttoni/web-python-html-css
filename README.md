8bf0f601518624b3de18800c62044d803f66877f
Angelina Meiras Ottoni <angel.m.ottoni@gmail.com>
2021-09-13 19:58:48 +0000

    atualização do CSS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# py-html-css
+# Programando para web com Python, CSS e HTML (Parte 1, 2, 3)
+*com Prof. Guilherme Feulo*
+
+- Foi realizada uma implementação do python com o HTML e CSS, para a construção de um blog. Por meio do curso foi possível entender como a mistura de linguagens funciona e como saber se organizar durante a construção de aplicações, tirando um pouco do medo que temos quando o assunto é combinar diferentes linguagens em uma única aplicação.
+
+*Curso realizado pela plataforma http://oceanbrasil.com/*
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, abort, render_template, flash
+from flask import Flask, request, url_for, session, g, redirect, abort, render_template, flash
 
 # configuração
-DATABASE = 'blog.db'
-SECRET_KEY = 'pudim'
+DATABASE = "blog.db"
+SECRET_KEY = 'mimiu'
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -12,7 +13,7 @@ def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
 
 @app.before_request
-def antes_requisisao():
+def antes_requisicao():
     g.bd = conectar_bd()
 
 @app.teardown_request
@@ -20,9 +21,39 @@ def depois_request(exc):
     g.bd.close()
 
 @app.route('/')
+@app.route('/entradas')
 def exibir_entradas():
-    return render_template('exibir_entradas.html')
+    sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, texto in cur.fetchall():
+        entradas.append({'titulo': titulo, 'texto': texto})
+    return render_template('exibir_entradas.html', entradas=entradas)
 
-@app.route('/hello')
-def pagina_inicial():
-    return "Hello World"
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?,?)"
+    g.bd.execute(sql, (request.form['campoTitulo'], request.form['campoTexto']))
+    g.bd.commit()
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = ''
+    if request.method == "POST":
+        if request.form['campoUsuario'] != 'maria-sissi' \
+        or request.form['campoSenha'] != 'mimiu':
+            erro = "Senha ou Usuário Inválidos"
+        else:
+            session['logado'] = True
+            return redirect(url_for('exibir_entradas'))
+
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('Logout OK')
+    return redirect(url_for('exibir_entradas'))
